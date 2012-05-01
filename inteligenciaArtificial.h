cbe8fa3071f5ef63480a2daf8217630aa3e02a39
Yuri Pelz Gossmann <yuripg1@gmail.com>
2012-05-01 18:45:50 -0300

    Implementacao da poda alfa-beta (commit anterior)

--- inteligenciaArtificial.h
+++ inteligenciaArtificial.h
@@ -8,7 +8,6 @@
 #define EMPATE 1
 #define CONTINUA 2
 #define VITORIA 3
-#define SEM_ALFA_BETA (-1)
 int jogadaComputador(int *tabuleiro,int corComputador,int profundidadeMinimax,int jogadasFeitas);
 char minimax(int *tabuleiro,char profundidade,char corAtual,char jogadasFeitas,char alfaBeta);
 int primeiroVazio(int *tabuleiro);
