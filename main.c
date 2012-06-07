b45246238d15af14c88190b7f4e9c0add76fec90
Yuri Pelz Gossmann <yuripg1@gmail.com>
2012-06-07 00:22:25 -0300

    Adicao de controle de prioridade

--- main.c
+++ main.c
@@ -2,7 +2,11 @@
 #include "constantes.h"
 #include "jogo.h"
 int main(){
+#ifdef ALTA_PRIORIDADE
 	SetPriorityClass(GetCurrentProcess(),HIGH_PRIORITY_CLASS);
+#else
+	SetPriorityClass(GetCurrentProcess(),IDLE_PRIORITY_CLASS);
+#endif
 	jogo();
 	return 0;
 }
